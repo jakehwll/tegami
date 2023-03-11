69424c7ea0365683cefba836852cb58c81d0ed7b
Jake <jake@hwll.me>
2023-03-11 17:55:09 +1100

    fix: resolve scraping script

---
+++ apps/scheduler/util/database.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "database";
+
+const database = new PrismaClient({
+  log: [
+    {
+      emit: "stdout",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+export default database;
